13695d95d5ac5e5d53d6d082691f89a9b4cf3a52
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2018-08-21 10:08:29 +0700

    fix json

--- routes/local.js
+++ routes/local.js
@@ -7,10 +7,10 @@ router.get('/local',(req,res,next)=>{
 });
 
 router.post('/local', async (req,res,next)=>{
-    console.log(req.body);
+    console.log(req.body.Local.start_location);
     await new Coordinates({
-        start_lat : req.body.Local.start_localtion.lat,
-        start_lng : req.body.Local.start_localtion.lng,
+        start_lat : req.body.Local.start_location.lat,
+        start_lng : req.body.Local.start_location.lng,
         end_lat : req.body.Local.end_location.lat,
         end_lng : req.body.Local.end_location.lng,
     }).save(); 
