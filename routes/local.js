a118d02cad16319d0f31ad1456b3c20220421db9
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2018-08-21 21:35:22 +0700

    bo data

--- routes/local.js
+++ routes/local.js
@@ -8,12 +8,7 @@ router.get('/local',(req,res,next)=>{
 
 router.post('/local', async (req,res,next)=>{
     console.log(req.body);
-    // await new Coordinates({
-    //     start_lat : req.body.Local.start_location.lat,
-    //     start_lng : req.body.Local.start_location.lng,
-    //     end_lat : req.body.Local.end_location.lat,
-    //     end_lng : req.body.Local.end_location.lng,
-    // },).save(); 
+    
       res.json(req.body);
 });
 
