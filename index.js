aae110cc9c0d47989035e5bc16377791d9277174
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2018-08-22 10:08:24 +0700

    sua form api

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use('/api',local);
 app.get('/',(req,res)=>{
     res.json({title: "Hello"});
 });
+
 http.createServer(app).listen(80, '0.0.0.0',
   function(){
     console.log("Express server listening on port " + app.get('port'));
