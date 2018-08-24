fc6fcb2db0f5fa5628cdd9f92d653969d22e05c6
ruanshiron <sandt26@gmail.com>
2018-08-24 22:45:47 +0700

    Add Code "Add Data"

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var http = require('http');
+
 var app = express();
 var mongoose = require ('mongoose');
 
 var local = require("./routes/local");
 
 //connect data
-mongoose.connect('mongodb://vinhyenvodoi98:vinhyenvodoi98@ds125602.mlab.com:25602/hackathon2018',{ useNewUrlParser: true });
+mongoose.connect('mongodb://vinhpro1998:1998vinhpro@ds125362.mlab.com:25362/vinhnodb',{ useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended :false}));
@@ -19,7 +20,7 @@ app.get('/',(req,res)=>{
     res.json({title: "Hello"});
 });
 
-http.createServer(app).listen(80, '0.0.0.0',
+http.createServer(app).listen(3000, '0.0.0.0',
   function(){
     console.log("Express server listening on port " + app.get('port'));
 });
--- routes/local.js
+++ routes/local.js
@@ -1,227 +1,131 @@
 var express = require('express');
-var Coordinates1 = require ('../models/Coordinates');
+var Coordinates = require ('../models/Coordinates');
+var mlways = require ('../models/mlways');
+var Waypoints = require ('../models/waypoints');
+
 var router = express.Router();
 
 router.get('/local',(req,res,next)=>{
-    res.json({
-        Instruct: {
-            step: [
-                {
-                    start_location: {
-                        lat: "21.0019937",
-                        lng: "105.8521863"
-                    },
-                    end_location: {
-                        lat: "21.0022207",
-                        lng: "105.8508119"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "21.0022207",
-                        lng: "105.8508119"
-                    },
-                    end_location: {
-                        lat: "20.9958565",
-                        lng: "105.8499201"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "20.9958565",
-                        lng: "105.8499201"
-                    },
-                    end_location: {
-                        lat: "20.9972627",
-                        lng: "105.8426467"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "20.9972627",
-                        lng: "105.8426467"
-                    },
-                    end_location: {
-                        lat: "21.0031025",
-                        lng: "105.8203995"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "21.0031025",
-                        lng: "105.8203995"
-                    },
-                    end_location: {
-                        lat: "21.0154221",
-                        lng: "105.8050682"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "21.0154221",
-                        lng: "105.8050682"
-                    },
-                    end_location: {
-                        lat: "21.0134991",
-                        lng: "105.8026049"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "21.0134991",
-                        lng: "105.8026049"
-                    },
-                    end_location: {
-                        lat: "21.0231891",
-                        lng: "105.7901765"
-                    }
-                },
-                {
-                    start_location: {
-                        lat: "21.0231891",
-                        lng: "105.7901765"
-                    },
-                    end_location: {
-                        lat: "21.0272930",
-                        lng: "105.7877501"
-                    }
-                }
-                // {
-                //     start_location: {
-                //         lat: "21.0272930",
-                //         lng: "105.7877501"
-                //     },
-                //     end_location: {
-                //         lat: "21.0265990",
-                //         lng: "105.7866829"
-                //     }
-                // }
-            ]
-        }
-    })
+    console.log(req)
+    
+    let start_lat = Number(req.query.start_lat)
+    let start_lng = Number(req.query.start_lng)
+    let end_lat = Number(req.query.end_lat)
+    let end_lng = Number(req.query.end_lng)
+
+    console.log(start_lat,start_lng,end_lat,end_lng)
+    
+    res.json({})
+    
 });
 
 router.post('/local', async (req,res)=>{
     console.log(req.body);
-    await new Coordinates1({
+    var abc = Array(req.body.Local.start_location);
+    abc.length
+    console.log(req.body.Local.start_location.length);
+    await new Coordinates({
+
         start_lat : req.body.Local.start_location[0].lat,
         start_lng : req.body.Local.start_location[0].lng,
         end_lat : req.body.Local.end_location[0].lat,
         end_lng : req.body.Local.end_location[0].lng,
-    }).save()
-    .then(doc=>{
-        res.json({
-            Instruct: {
-                step: [
-                    {
-                        start_location: {
-                            lat: "21.0019937",
-                            lng: "105.8521863"
-                        },
-                        end_location: {
-                            lat: "21.0022207",
-                            lng: "105.8508119"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0022207",
-                            lng: "105.8508119"
-                        },
-                        end_location: {
-                            lat: "20.9958565",
-                            lng: "105.8499201"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "20.9958565",
-                            lng: "105.8499201"
-                        },
-                        end_location: {
-                            lat: "20.9972627",
-                            lng: "105.8426467"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "20.9972627",
-                            lng: "105.8426467"
-                        },
-                        end_location: {
-                            lat: "21.0031025",
-                            lng: "105.8203995"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0031025",
-                            lng: "105.8203995"
-                        },
-                        end_location: {
-                            lat: "21.0154221",
-                            lng: "105.8050682"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0154221",
-                            lng: "105.8050682"
-                        },
-                        end_location: {
-                            lat: "21.0134991",
-                            lng: "105.8026049"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0134991",
-                            lng: "105.8026049"
-                        },
-                        end_location: {
-                            lat: "21.0231891",
-                            lng: "105.7901765"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0231891",
-                            lng: "105.7901765"
-                        },
-                        end_location: {
-                            lat: "21.0272930",
-                            lng: "105.7877501"
-                        }
-                    },
-                    {
-                        start_location: {
-                            lat: "21.0272930",
-                            lng: "105.7877501"
-                        },
-                        end_location: {
-                            lat: "21.0265990",
-                            lng: "105.7866829"
-                        }
-                    }
-                ]
-            }
-        });
-    })
-    .catch(err=>{
-        res.json(err);
-    })
+    }).save();
+        if(error) throw error;
+        res.json(req.body);
 });
 
 router.get('/maytram',(req,res)=>{
-    res.json({title: "tim cach post di"});
-    // const _arr = service.get(() =>  []);
-    // res.json(_arr)
+    res.json({title : 'tim cach ket noi nhe'});
 })
 
-router.post('/maytram',async (req,res)=>{
+router.post('/maytram', (req,res)=>{
     console.log(req.body);
-    ///
+    /// TODO
+
+    let all = String(req.body.all)
+    let line = all.split('\n')
+
+    for (var i = 0; i < line.length; i++) {
+        // 1	1	2	140	5
+        // 2	2	3	190	6
+        // 3	3	4	190	7
+        // 4	4	5	210	8
+        // 5	5	6	110	9
+        // 6	6	7	200	10
+        // 7	7	8	120	11
+        // 8	8	9	110	12
+        let each = line[i].split('\t')
+        let id = each[0]
+        let startWaypointID = each[1]
+        let endWaypointI = each[2]
+        let distant = each[3]
+        let time = each[4]
+
+        //console.log(time)
+        
+
+        var mlway = new mlways({
+            _id: Number(id),
+            startWaypointID: Number(startWaypointID),
+            endWaypointID: Number(endWaypointI),
+            distant: Number(distant),
+            time: Number(time),
+        })
+
+        // mlway.findOneAndUpdate({"_id":Number(id)}, 
+        //                         {"startWaypointID": Number(startWaypointID),
+        //                         "endWaypointID": Number(endWaypointI),
+        //                         "distant": Number(distant),
+        //                         "time": Number(time),}, 
+        //                         (err,doc,res) => {
+        //                             if (err) {
+        //                                 mlway.save()
+        //                                 throw err
+        //                             }
+        //                         })
+        
+    }
+
+
+    res.json(req.body)
+})
+
+router.post('/datawaypoint', async (req,res) => {
+    //1	21.027233, 105.787644	"Unnamed RoadCầu Giấy, Hà Nội, Việt Nam"
+    let all = String(req.body.all)
+    let line = all.split('\"\n')
+
+    for (var i = 0; i < line.length; i++) {
+        let each = line[i].split('\t')
+        let id = each[0]
+        let place = each[2].replace('\"','')
+        let point = each[1].split(',')
+        let lat = point[0].replace('\"','')
+        let lng = point[1].replace('\"','')
+
+        await new Waypoints({
+                _id: Number(id),
+                place: String(place),
+                lat: Number(lat),
+                lng: Number(lng)
+            }).save() 
+    }
+    
+    console.log(line)
+
     res.json(req.body)
 })
 
 module.exports= router;
+
+// 1	21.027233, 105.787644	"Unnamed Road
+// Cầu Giấy, Hà Nội, Việt Nam"
+// 3	21.024640, 105.789153	"Shop dầu tràm
+// D29 Phạm Văn Bạch, Yên Hoà,"
+// 2	21.026233, 105.788228	"Unnamed Road
+// Yên Hoà, Cầu Giấy, Hà Nội, Việt Na"
+// 4	21.023266, 105.790010	"Dương Đình Nghệ
+// Yên Hoà, Cầu Giấy, Hà Nội, Việt Na"
+// 5	21.021621, 105.791132	"Công Ty Cổ Phần NaBo Việt Nam
+// 210 Trung Kính, Cầu Giấy, Yên Hoà"
