e45f3d50506909a752dd843eab75c977e980b616
hoang <vinhyenvodoi98@gmail.com>
2018-08-27 19:45:13 +0700

    update ver_1.1

--- timduong/jams.js
+++ timduong/jams.js
@@ -5,25 +5,32 @@ async function Jams(){
     var ratio ;
     let a = await mlways.find({ratio : {$lt: 0.3}});
     //console.log(a[0].startWaypointID);
-    let b = await Waypoints.find({_id: a[0].startWaypointID});
-    let c = await Waypoints.find({_id: a[0].endWaypointID});
-    // console.log(b);
-    // console.log(c);
-
-    var x = a.map((point) => {
-        return {
-            start_location: {
-                lat : b[0].lat,
-                lng : b[0].lng
-            },              
-            end_location: {
-                lat : c[0].lat,
-                lng : c[0].lng
+    console.log(a);
+    if(a.length == 0){
+        return a;
+    }else{
+        console.log('ko dc');
+        let b = await Waypoints.find({_id: a[0].startWaypointID});
+        let c = await Waypoints.find({_id: a[0].endWaypointID});
+        // console.log(b);
+        // console.log(c);
+    
+        var x = a.map((point) => {
+            return {
+                start_location: {
+                    lat : b[0].lat,
+                    lng : b[0].lng
+                },              
+                end_location: {
+                    lat : c[0].lat,
+                    lng : c[0].lng
+                }
             }
-        }
-    })
-
-    return x;
+        })
+    
+        return x;
+    }
+    
 }
 
 module.exports = Jams;
